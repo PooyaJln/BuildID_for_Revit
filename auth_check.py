baf16909972809c8c720a4693484e06b829e5188
PooyaJln <p.j@mailbox.org>
2023-06-11 00:44:09 +0200

    completed auth_check
    auth_check.py checks whether .env file exists or not.
    If not it asks the use for the authentication data and
    creates the file.

--- auth_check.py
+++ auth_check.py
@@ -1,12 +1,18 @@
 import os
-import sys
 
-filename = ".env1"  
+file_name = ".env"
 
-if os.path.exists(filename):
-    print("File exists in the current directory.")
-else:
-    print("File does not exist in the current directory.")
-    with open(filename, "w") as file:
-        
-        print("File created.")
+
+def env_file_check():
+    if not os.path.exists(file_name):
+        print("A file required for saving API login data does not exist.")
+        API_BASE_URI = input('Enter API address: ')
+        USERNAME = input('\nEnter the username: ')
+        PASSWORD = input('\nEnter the password: ')
+        with open(file_name, "w") as file:
+            # file.write('API_BASE_URI=\"https://buildid-vasakronan.azurewebsites.net/itemlistexport\"\nUSERNAME=huginbiuser\nPASSWORD=V55qyyJ3LgQKTkqNKgbZ')
+            file.write(
+                f'API_BASE_URI={API_BASE_URI}\nUSERNAME={USERNAME}\nPASSWORD={PASSWORD}')
+            print("File created.")
+    else:
+        return True
