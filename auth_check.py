b1da729af39ca65e44e083cf2670e6a31f34daa9
Pooya Jln <p.j@mailbox.org>
2023-06-12 15:57:20 +0200

    Removed the hard-coded endpoint

--- auth_check.py
+++ auth_check.py
@@ -10,7 +10,6 @@ def env_file_check():
         USERNAME = input('\nEnter the username: ')
         PASSWORD = input('\nEnter the password: ')
         with open(file_name, "w") as file:
-            # file.write('API_BASE_URI=\"https://buildid-vasakronan.azurewebsites.net/itemlistexport\"\nUSERNAME=huginbiuser\nPASSWORD=V55qyyJ3LgQKTkqNKgbZ')
             file.write(
                 f'API_BASE_URI={API_BASE_URI}\nUSERNAME={USERNAME}\nPASSWORD={PASSWORD}')
             print("File created.")
